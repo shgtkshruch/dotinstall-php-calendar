e2dee399b4cbb97afa3beeef2b97a0d9ece434c7
shgtkshruch <shgtk.shruch@gmail.com>
2015-01-15 08:54:10 +0900

    Add index.php

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+  <title>Calendor with PHP</title>
+  <link rel="stylesheet" href="style.css" media="all">
+</head>
+<body>
+  <table>
+    <thead>
+      <tr>
+        <th><a href="">&laquo;</a></th>
+        <th colspan="5">2013-07</th>
+        <th><a href="">&raquo;</a></th>
+      </tr>
+      <tr>
+       <th>日</th>
+       <th>月</th>
+       <th>火</th>
+       <th>水</th>
+       <th>木</th>
+       <th>金</th>
+       <th>土</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+       <td>1</td>
+       <td>2</td>
+       <td>3</td>
+       <td>4</td>
+       <td>5</td>
+       <td>6</td>
+       <td>7</td>
+      </tr>
+    </tbody>
+  </table>
+</body>
+</html>
